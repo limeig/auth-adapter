9a8053b05c75768d370e1c099b023b245d9fcb04
Alina <lina.tatarova@mail.ru>
2024-10-05 23:23:16 +0300

    Start to implement api to add child and log in assignment results

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as sdk from "@basaldev/blocks-backend-sdk";
 import { defaultAdapter, UserAppConfig, createNodeblocksUserApp } from "@basaldev/blocks-user-service";
 
+import { MongoClient } from 'mongodb';
+
 /**
  * Access to the configs set on the NBC dashboard based no the adapter manifest(nbc.adapter.json) by process.env
  * 
@@ -128,7 +130,7 @@ export function beforeCreateService(currentConfigs: UserAppConfig): UserAppConfi
  * A hook function called after the service is created
  * This hook can be used to perform any post service creation tasks
  */
-export function serviceCreated() {}
+export function serviceCreated() { }
 
 type StartServiceArgs = Parameters<ReturnType<typeof createNodeblocksUserApp>['startService']>;
 type ServiceOpts = StartServiceArgs[0];
@@ -171,6 +173,94 @@ export function beforeStartService(currentOptions: ServiceOpts): StartServiceArg
    */
   const updatedOptions = {
     ...currentOptions,
+    customRoutes: [
+      {
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const client = new MongoClient(process.env.ADAPTER_DATABASE_URL);
+          try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+            // Make the appropriate DB calls
+            const db = client.db("izumo1_mvp");
+
+            let child = {
+              first_name: context.body["child_first_name"],
+              birthday: context.body["child_birthday"],
+              Subjects: context.body["subjects_ids"],
+              Reviews: undefined
+            };
+
+            await db.collection("children").insertOne(child);
+            let parent_id = { id: context.body["parent_id"] };
+
+            await db.collection("users").updateOne(parent_id, {
+              $addToSet: {
+                Children: child["id"]
+              }
+            });
+          } catch (e) {
+            console.error(e);
+          } finally {
+            await client.close();
+          }
+          return {
+            data: undefined,
+            status: 200
+          };
+        },
+        method: 'post' as const,
+        path: '/children/create',
+        validators: [
+          async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+            //  context.body
+            return 200;
+          }
+        ]
+      },
+      {
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const client = new MongoClient(process.env.ADAPTER_DATABASE_URL);
+          try {
+            // Connect to the MongoDB cluster
+            await client.connect();
+            // Make the appropriate DB calls
+            const db = client.db("izumo1_mvp");
+
+            let review = {
+              date:    context.body["date"],
+              Subject: context.body["subject"],
+              hours:   context.body["hours"],
+              Asessment: undefined
+            };
+
+            await db.collection("reviews").insertOne(review);
+            let child_id = { id: context.body["child_id"] };
+
+            await db.collection("children").updateOne(child_id, {
+              $addToSet: {
+                Reviews: review["id"]
+              }
+            });
+          } catch (e) {
+            console.error(e);
+          } finally {
+            await client.close();
+          }
+          return {
+            data: undefined,
+            status: 200
+          };
+        },
+        method: 'post' as const,
+        path: '/children/log_assignment',
+        validators: [
+          async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+            //  context.body
+            return 200;
+          }
+        ]
+      }
+    ]
   };
   return [updatedOptions];
 }
@@ -179,4 +269,4 @@ export function beforeStartService(currentOptions: ServiceOpts): StartServiceArg
  * A hook function called after the service is started
  * This hook can be used to perform any post service starting tasks
  */
-export function serviceStarted() {}
+export function serviceStarted() { }

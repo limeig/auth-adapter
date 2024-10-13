77d9a173283c0c3f7b2adce052c0a2bcdb67f3d9
Alina <lina.tatarova@mail.ru>
2024-10-13 16:05:01 +0300

    Add migration code and initiate migration when service is started

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as sdk from "@basaldev/blocks-backend-sdk";
 import { defaultAdapter, UserAppConfig, createNodeblocksUserApp } from "@basaldev/blocks-user-service";
 import * as handlers from  "./handlers/handlers";
-
+import migrate from "../db_migrations/migrations"
 /**
  * Access to the configs set on the NBC dashboard based no the adapter manifest(nbc.adapter.json) by process.env
  * 
@@ -115,7 +115,9 @@ export function beforeCreateService(currentConfigs: UserAppConfig): UserAppConfi
  * A hook function called after the service is created
  * This hook can be used to perform any post service creation tasks
  */
-export function serviceCreated() {}
+export function serviceCreated() {
+  migrate();
+}
 
 type StartServiceArgs = Parameters<ReturnType<typeof createNodeblocksUserApp>['startService']>;
 type ServiceOpts = StartServiceArgs[0];
